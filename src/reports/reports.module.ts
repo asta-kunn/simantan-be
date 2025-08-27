83bbac9810ae2b0d609a26db213385d9fd67b67a
Muhammad Rifqi Adli Gumay <muhammad.gumay-iu@dexagroup.com>
2025-08-27 09:47:17 +0700

    rebuild

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -12,4 +12,5 @@ import { SubmissionDocument } from './entities/submission-document.entity';
   controllers: [ReportsController],
   providers: [ReportsService],
 })
+
 export class ReportsModule {}
