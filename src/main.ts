6afb500d5b7fbba45f3a7e2e881e6a4cdfb1897a
asta-kunn <rifqibradley12@gmail.com>
2026-06-30 10:27:32 +0700

    fix vercel

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common'; 
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+// Buat instance express di luar untuk di-export ke Vercel
+const server = express.default();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Gunakan ExpressAdapter agar NestJS menempel ke instance express di atas
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   
   app.enableCors({
     origin: [
@@ -20,6 +27,16 @@ async function bootstrap() {
   
   app.useGlobalPipes(new ValidationPipe()); 
   
-  await app.listen(process.env.PORT || 8080);
+  // Jika berjalan di lokal, gunakan listen. Jika di Vercel, gunakan init saja
+  if (process.env.NODE_ENV !== 'production') {
+    await app.listen(process.env.PORT || 8080);
+  } else {
+    await app.init();
+  }
 }
+
+// Jalankan fungsi bootstrap
 bootstrap();
+
+// EXPORT server ini agar Vercel bisa membacanya
+export default server;
